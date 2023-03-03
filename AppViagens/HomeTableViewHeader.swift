8c3fa1c4bb3538e13aaea5b27e8a4d7875223d3a
Lucas Napoleão <102775417+luknapoleao@users.noreply.github.com>
2023-03-02 22:37:59 -0300

    AppViagens
    version: 1.0

---
+++ AppViagens/HomeTableViewHeader.swift
@@ -0,0 +1,25 @@
+//
+//  HomeTableViewHeader.swift
+//  AppViagens
+//
+//  Created by Lucas Napoleao arantes de sousa on 02/03/23.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+// MARK: -IBOutlets
+    
+    
+    @IBOutlet weak var tituloLabel:
+         UILabel!
+    @IBOutlet weak var headerView:
+         UIView!
+    
+    @IBOutlet weak var bannerImageView:
+         UIImageView!
+    
+    
+    
+}
